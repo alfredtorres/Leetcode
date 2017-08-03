1f788c275f2ab5c030ec827bfe909f66445b1844
ALFRED_1993 <alfred_torres@163.com>
2017-08-03 09:34:29 +0800

    BubbleSort_InerstSort_changes
    changes

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -10,6 +10,10 @@ void Swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
+/*
+将最大元素一到数组最后
+没有进行改进
+*/
 void BubbleSort(int* a, int length)
 {
 	for (int i = 0; i < length; i++)
@@ -21,6 +25,28 @@ void BubbleSort(int* a, int length)
 		}
 	}
 }
+/*
+将最小元素移到数组最前
+进行改进
+*/
+void BubbleSort1(int* a, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		bool isSorted = true;
+		for (int j = length - 1; j>i; j--)
+		{
+			if (*(a + j) < *(a + j - 1))
+			{
+				int temp = *(a + j - 1);
+				*(a + j - 1) = *(a + j);
+				*(a + j) = temp;
+				isSorted = false;
+			}
+		}
+		if (isSorted) return;
+	}
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int a[] = { 89, 45, 68, 90, 29, 34, 17 };
@@ -29,7 +55,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < length; i++)
 		cout << a[i] << " ";
 	cout << endl;
-	BubbleSort(a, length);
+	BubbleSort1(a, length);
 	cout << "选择排序后数组：" << endl;
 	for (int i = 0; i < length; i++)
 		cout << a[i] << " ";
--- InsertionSort/InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort/InsertionSort.cpp
@@ -31,7 +31,7 @@ void InsertionSort(int* array,int length)
 		}
 	}
 }
-/*插入排序
+/*直接插入排序
 上面的代码复杂，为了减少移动次数，应该从右往左扫描表
 */
 void InsertionSort2(int* array, int length)
@@ -48,6 +48,50 @@ void InsertionSort2(int* array, int length)
 		*(array + j + 1) = tmp;
 	}
 }
+/*折半插入排序
+将表中寻找的过程，用二分查找代替，加快查找的速度
+*/
+void InsertionSortMid(int* array, int length)
+{
+	int i, j, start, mid, end;
+	for (i = 1; i < length; i++)
+	{
+		int temp = *(array + i);
+		start = 0;
+		end = i - 1;
+		while (start<=end)
+		{
+			mid = (start + end) / 2;
+			if (*(array + mid) < temp)
+				start = mid + 1;
+			if (*(array + mid) > temp)
+				end = mid - 1;
+		}
+		for (j = i - 1; j >= end + 1; j--)
+			*(array + j + 1) = *(array + j);
+		*(array + end + 1) = temp;
+	}
+}
+void ShellSort(int* array, int length)
+{
+	int gap[] = { 4, 1 };
+	int i, j, k;
+	for ( i = 0; i < 2; i++)
+	{
+		int step = gap[i];
+		for ( j = step; j < length; j++)
+		{
+			int temp = *(array + j);
+			k = j - 1;
+			while (k >= 0 && temp < *(array + k))
+			{
+				*(array + k + 1) = *(array + k);
+				k--;
+			}
+			*(array + k + 1) = temp;
+		}
+	}
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int a[] = { 89, 45, 68, 90, 29, 34, 17 };
@@ -56,7 +100,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < length; i++)
 		cout << a[i] << " ";
 	cout << endl;
-	InsertionSort2(a, length);
+	ShellSort(a, length);
 	cout << "选择排序后数组：" << endl;
 	for (int i = 0; i < length; i++)
 		cout << a[i] << " ";
